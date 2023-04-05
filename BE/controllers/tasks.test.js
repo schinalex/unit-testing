1abfcaa5e4d3e71f2b45a5c1f258abf1c2f8c97f
Alexandru Schin <Alex.Schin@endava.com>
2023-04-05 15:13:45 +0300

    write tests for getAllTasks

--- BE/controllers/tasks.test.js
+++ BE/controllers/tasks.test.js
@@ -1,12 +1,32 @@
 const { getAllTasks } = require('./tasks')
+const Task = require('../models/Task')
 
 describe('tasks', () => {
   describe('getAllTasks', () => {
-    it.skip('should get all tasks', () => {
-      const check = {json: jest.fn()}
-      return getAllTasks({}, {status:() => check}).then(() => {
-        expect(check.json).toHaveBeenCalledWith({})
-      })
+    it('should get all tasks', async () => {
+      const req = {}
+      const res = {
+        status: jest.fn(() => res),
+        json: jest.fn()
+      }
+      const resultOfFind = [{task: 'do the dishes'}]
+      jest.spyOn(Task, 'find').mockImplementation(jest.fn(() => Promise.resolve(resultOfFind)))
+      await getAllTasks(req, res)
+      expect(Task.find).toBeCalledWith({})
+      expect(res.status).toBeCalledWith(200)
+      expect(res.json).toBeCalledWith(resultOfFind)
+    })
+    it('should handle errors', async () => {
+      const req = {}
+      const res = {
+        status: jest.fn(() => res),
+        json: jest.fn()
+      }
+      const error = Error('WOW, such error')
+      jest.spyOn(Task, 'find').mockImplementation(jest.fn(() => Promise.reject(error)))
+      await getAllTasks(req, res)
+      expect(res.status).toBeCalledWith(500)
+      expect(res.json).toBeCalledWith(error)
     })
   })
 })
